024f3bb82d0edee19e76e8f4b397ac1e146055cc
ShashimaniDwivedi <shashimanidwivedi6@gmail.com>
2023-08-07 18:29:32 +0530

    Searching

--- Searching/src/selectionSort.java
+++ Searching/src/selectionSort.java
@@ -5,7 +5,7 @@ public class selectionSort {
     public static void selectionSort(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             int current = i;
-            for (int j = i+1; j < arr.length - 1; j++) {
+            for (int j = i+1; j < arr.length; j++) {
                 if (arr[current] > arr[j]) {
                     //int temp = arr[current];
                     //arr[current] = arr[j];
