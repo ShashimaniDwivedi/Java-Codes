64bae1fc754c82defd0782391c909904d776dda1
ShashimaniDwivedi <shashimanidwivedi6@gmail.com>
2023-09-04 19:43:23 +0530

    PatternProgramming

---
+++ PatternProgramming/src/pattern.java
@@ -0,0 +1,49 @@
+public class pattern {
+        public static void main(String[] args) {
+            int n = 7;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(j==0||j==n-1||i==j&&i<=n/2||i+j==n-1&&i<=n/2) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  "); // Two spaces for separation
+                    }
+                }
+                System.out.print(" ");
+                for (int j = 0; j < n; j++) {
+                    if(j==0 && i>0||j==n-1 &&i>0||i==0 &&j>0 && j<n-1 ||i==n/2){
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  "); // Two spaces for separation
+                    }
+                }
+                System.out.print(" ");
+                for (int j = 0; j < n; j++) {
+                    if(j==0 ||j==n-1||i==j){
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  "); // Two spaces for separation
+                    }
+                }
+                System.out.print(" ");
+                for (int j = 0; j < n; j++) {
+                    if(j==0 && i>0||j==n-1 &&i>0||i==0 &&j>0 && j<n-1 ||i==n/2){
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  "); // Two spaces for separation
+                    }
+                }
+                System.out.print(" ");
+                for (int j = 0; j < n; j++) {
+                    if(i==0||i==n-1||i==n/2||j==0&&i<=n/2||j==n-1&&i>=n/2){
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  "); // Two spaces for separation
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
