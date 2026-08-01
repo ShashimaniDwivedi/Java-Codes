32c825de968d72a321b05853c15878a036f18b34
Shashi <shashimanidwivedi6@gmail.com>
2026-08-01 14:08:22 +0530

    Autoboxing and unboxing

---
+++ Boxing.java
@@ -0,0 +1,12 @@
+public class Boxing {
+    public static void main(String[] args) {
+        Integer x=40,y=40;
+        if(x==y)System.out.println("Same");
+        else System.out.println("Not Same");
+        // o/p Same
+        Integer a=400,b=400;
+        if(a==b)System.out.println("Same");
+        else System.out.println("Not Same");
+        //o/p Not Same
+    }
+}
