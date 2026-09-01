a43a05eeb76c409ad79542a067d4022140aaf02d
Shashi <shashimanidwivedi6@gmail.com>
2026-09-01 21:04:17 +0530

    Inner class

---
+++ Inner.java
@@ -0,0 +1,23 @@
+class A{
+    
+    public void show(){
+        System.out.println("in show");
+    }
+
+
+    class B{
+        public void config(){
+            System.out.println("in config");
+        }
+    }
+}
+
+
+public class Inner {
+    public static void main(String[] args) {
+        A obj=new A();
+        obj.show();
+        A.B x=obj.new B();
+        x.config();
+    }
+}
